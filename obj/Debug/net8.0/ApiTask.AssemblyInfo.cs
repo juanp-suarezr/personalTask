788036d547069221b1575690c506e7530b0c681a
juanp-suarezr <juanp-suarezr@users.noreply.github.com>
2025-10-08 09:10:45 -0500

    actu

--- obj/Debug/net8.0/ApiTask.AssemblyInfo.cs
+++ obj/Debug/net8.0/ApiTask.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiTask")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d558a7df4a3a6d247fdec135873b0e37026a54a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41efca2664f1912d4dcbed0a68deb39f8ec3f010")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiTask")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiTask")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
